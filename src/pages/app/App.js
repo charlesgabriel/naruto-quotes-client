b19f90a347f434dda2c0157386fb81f62715f606
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-04-21 12:57:43 -0300

    projeto inicial

---
+++ src/pages/app/App.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react';
+import { GlobalStyle } from '../../components';
+
+export function App() {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <div>Hello</div>
+    </Fragment>
+  );
+}
