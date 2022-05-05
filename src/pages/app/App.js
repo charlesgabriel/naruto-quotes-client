3c55d264b4bdf052ab97cef0fe26d55c001169b2
Charles Gabriel <charlesgabriel@hotmail.com.br>
2022-05-05 11:15:15 -0300

    finalizado uso de hook useref/done using useref hook

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 import narutoImg from '../../images/naruto.png';
 import { Quotes } from '../../components';
@@ -8,18 +8,24 @@ import jutsoSound from '../../sounds/jutso.mp3';
 const audio = new Audio(jutsoSound);
 
 export function App() {
+  let isMounted = useRef(true);
   const [ quoteState, setQuoteState ] = useState({
-    quote: 'ok', speaker: 'Speaker'
+    quote: 'loading quote...', speaker: 'loading speaker'
   });
 
   const onUpdate = async () => {;
     const quote = await getQuote();
-    audio.play();
-    setQuoteState(quote);
+    if(isMounted.current) {
+      audio.play();
+      setQuoteState(quote);
+    }
   };
-
+  
   useEffect(() => {
     onUpdate();
+    return () => {
+      isMounted.current = false;
+    }
   }, []);
 
   return (
